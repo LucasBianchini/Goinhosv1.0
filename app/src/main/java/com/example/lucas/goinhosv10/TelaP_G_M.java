6077d659cf5b9f47f951bce4c90de918387dc5c4
LucasBianchini <lucas_bianchini1@hotmail.com>
2017-04-12 17:20:55 -0300

    Initial commit

---
+++ app/src/main/java/com/example/lucas/goinhosv10/TelaP_G_M.java
@@ -0,0 +1,13 @@
+package com.example.lucas.goinhosv10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaP_G_M extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_p__g__m);
+    }
+}
